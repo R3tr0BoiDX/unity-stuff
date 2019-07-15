38bf13e1d672fecade2c90409d5c4fd9584bcc48
Mirbro <mircojanisch@hotmail.com>
2019-07-15 01:14:54 -0700

    Added a SimpleAnimator.cs and editor script
    Added a simple lightweight animator I used in quite a few project, coming with a handy, not completely perfect Editor script.
    
    Know issues:
    -Editor script playback seems off. The jump from last to first frame is not quite right. But works fine for testing through.

---
+++ SimpleAnimator.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleAnimator : MonoBehaviour {
+	[SerializeField] private bool GUIMode;
+	[SerializeField] private bool startAtSpawn = true;
+	[SerializeField] private bool loopAnimation;
+	[SerializeField] private bool destroyAfterFinished;
+	[SerializeField] private float destroyDelay;
+	[SerializeField] private float timeBetweenFrames;
+	[SerializeField] private Sprite[] frames;
+
+	private int i;
+	private bool play;
+	private Coroutine routine;
+	private SpriteRenderer rend;
+	private Image image;
+
+	public Sprite[] Frames {
+		get => frames;
+	}
+
+	public float TimeBetweenFrames {
+		get => timeBetweenFrames;
+	}
+
+	public bool useGUIMode {
+		get => GUIMode;
+		set => GUIMode = value;
+	}
+
+    private void Start() {
+		if (GUIMode) {
+			image = GetComponent<Image>();
+		} else {
+			rend = GetComponent<SpriteRenderer>();
+		}
+
+		if (startAtSpawn) {
+			play = true;
+			routine = StartCoroutine(Animate());
+		}
+	}
+
+	public void Play() {
+		if (!play) {
+			play = true;
+		}
+		routine = StartCoroutine(Animate());
+	}
+
+	public void Stop() {
+		StopCoroutine(routine);
+        SetSprite(frames[0]);
+    }
+
+    public void Pause() {
+		if (play) {
+			play = false;
+		}
+	}
+
+	private IEnumerator Animate() {
+		if (play) {
+			SetSprite(frames[i]);
+			i++;
+			yield return new WaitForSeconds(timeBetweenFrames);
+			if (i < frames.Length) {
+				routine = StartCoroutine(Animate());
+			} else if (loopAnimation) {
+				i = 0;
+				routine = StartCoroutine(Animate());
+			} else if (destroyAfterFinished) {
+				Destroy(gameObject, destroyDelay);
+			}
+		}
+	}
+
+	private void SetSprite(Sprite _sprite) {
+		if (GUIMode) {
+			image.sprite = _sprite;
+		} else {
+			rend.sprite = _sprite;
+		}
+	}
+}
---
+++ SimpleAnimatorEditor.cs
@@ -0,0 +1,85 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CustomEditor(typeof(SimpleAnimator))]
+public class SimpleAnimatorEditor : Editor {
+	private bool animate;
+	private int i;
+	private float lastEditorUpdate;
+	private float localTime;
+	private Sprite baseSprite;
+	private SimpleAnimator anim;
+	private SpriteRenderer rend;
+	private Image image;
+
+	private float resetDelay;
+	private bool delay;
+
+	public override void OnInspectorGUI() {
+		base.OnInspectorGUI();
+
+		anim = (SimpleAnimator)target;
+		if (anim.useGUIMode) {
+			image = ((MonoBehaviour)this.target).gameObject.GetComponent<Image>();
+		} else {
+			rend = ((MonoBehaviour)this.target).gameObject.GetComponent<SpriteRenderer>();
+		}
+
+		Rect rect = EditorGUILayout.GetControlRect(false, 1);
+		EditorGUI.DrawRect(rect, Color.grey);
+
+		GUIContent content = new GUIContent("Reset Delay", "Time til the animation resets");
+		resetDelay = EditorGUILayout.FloatField(content, resetDelay, EditorStyles.numberField);
+
+		if (GUILayout.Button("Animate")) {
+			if (anim.useGUIMode) {
+				baseSprite = image.sprite;
+			} else {
+				baseSprite = rend.sprite;
+			}
+			animate = true;
+		}
+	}
+
+	protected virtual void OnEditorUpdate() {
+		float deltaTime = Time.realtimeSinceStartup - lastEditorUpdate;
+		localTime += deltaTime;
+		if (animate && localTime > anim.TimeBetweenFrames) {
+			localTime = 0;
+			SetSprite(anim.Frames[i]);
+			i++;
+			if (i >= anim.Frames.Length) {
+				i = 0;
+				delay = true;
+				animate = false;
+			}
+		} else if (delay) {
+			if (localTime > resetDelay) {
+				SetSprite(baseSprite);
+				delay = false;
+			}
+		}
+		lastEditorUpdate = Time.realtimeSinceStartup;
+	}
+
+	private void SetSprite(Sprite _sprite) {
+		if (anim.useGUIMode) {
+			image.sprite = _sprite;
+		} else {
+			rend.sprite = _sprite;
+		}
+	}
+
+	protected virtual void OnEnable() {
+		EditorApplication.update += OnEditorUpdate;
+	}
+
+	protected virtual void OnDisable() {
+		EditorApplication.update -= OnEditorUpdate;
+	}
+}
+
+#endif
